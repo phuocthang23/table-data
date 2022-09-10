64ee51ad802ec79c13ce2c55473f82ccf55de01c
Your Name~ <you@example.com>
2022-09-10 18:40:34 +0700

    upcode

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <dataTable v-bind:list="list" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import dataTable from './components/dataTable.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    dataTable,
+  },
+  data() {
+    return {
+      list: [
+        { id:1, age: 21, first_name: "Larsen", last_name: "Shaw", salary: 2000000 },
+        { id:2, age: 89, first_name: "Geneva", last_name: "Wilson",salary: 3000000,},
+        { id:3, age: 40, first_name: "Dickerson", last_name: "Macdonald", salary: 1000000,},
+        { id:4, age: 38, first_name: "Jami", last_name: "Carney", salary: 4000000 },
+        { id:5, age: 23, first_name: "kami", last_name: "sama", salary: 5000000 },
+        { id:6, age: 27, first_name: "nami", last_name: "osaka", salary: 6000000 },
+        { id:7, age: 38, first_name: "Jami", last_name: "jame", salary: 7000000 },
+        { id:8, age: 45, first_name: "Jame", last_name: "Carney", salary: 8000000 },
+        { id:9, age: 19, first_name: "ami", last_name: "Carney", salary: 9000000 },
+      ],
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
